a8f30198ccf3059849a9f94d7d8497720efa7ac5
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-02-21 12:37:20 +0000

    soong: Allow 'python3.8'
    Required on archlinux when using venv
    
    Change-Id: I7b10e097559f8c7856ab3397a295bf920151b25c
    Signed-off-by: เจียน <jianrzy@naver.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -105,6 +105,7 @@ var Configuration = map[string]PathConfig{
 	"patch":    Allowed,
 	"pstree":   Allowed,
 	"python3":  Allowed,
+	"python3.8":  Allowed,
 	"realpath": Allowed,
 	"repo":     Allowed,
 	"rsync":    Allowed,
